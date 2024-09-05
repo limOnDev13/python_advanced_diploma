1f9baf737c95fb266fccfccdbaf6edbadcbcefaa
vladimir <volosnikovvladimir69@gmail.com>
2024-09-05 14:34:09 +0500

    Removed creation of engine, session and Base from database.py to models.py

--- src/database/models.py
+++ src/database/models.py
@@ -1,8 +1,18 @@
 """The module is responsible for the database models (tables)"""
 from sqlalchemy import Column, Text, Integer, String, ForeignKey
 from sqlalchemy.orm import relationship
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 
-from database import Base
+from src.config.config import load_config, Config
+
+
+config: Config = load_config()
+DB_URL: str = f"postgresql+asyncpg://{config.db.admin}:{config.db.password}@localhost"
+engine = create_async_engine(DB_URL)
+Session = sessionmaker(engine, expire_on_commit=False, class_=AsyncSession)
+Base = declarative_base()
 
 
 class User(Base):
