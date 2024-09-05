386ab78f98f9f877561c99abeddd5d716aa88a06
vladimir <volosnikovvladimir69@gmail.com>
2024-09-05 12:33:25 +0500

    Add a check for the existence of records in the User table. If there are no records, then a user with a test api key is added. Add doc-string for func create_new_tweet. Add a dependency function for getting a session. Change the returned object to JSONResponse

--- src/routes.py
+++ src/routes.py
@@ -1,9 +1,12 @@
 from contextlib import asynccontextmanager
 
-from fastapi import FastAPI
+from fastapi import FastAPI, status, Depends
+from fastapi.responses import JSONResponse
+from sqlalchemy.ext.asyncio import AsyncSession
 
-from schemas.schemas import Tweet
+from schemas import schemas
 from database.database import engine, Base, Session
+from database import queries as q
 
 
 @asynccontextmanager
@@ -11,13 +14,19 @@ async def lifespan(app_: FastAPI):
     # Startup
     async with engine.begin() as conn:
         await conn.run_sync(Base.metadata.create_all)
+
+    # Check that the user table has rows
+    session: AsyncSession = Session()
+    if not q.user_table_has_rows(session):
+        await q.create_user(session, {"api_key": "test_api_key"})
+
     yield
     # Shutdown
     await engine.dispose()
 
 
 async def get_session():
-    session = Session()
+    session: AsyncSession = Session()
     try:
         yield session
     finally:
@@ -28,6 +37,19 @@ app = FastAPI(lifespan=lifespan)
 
 
 @app.post('/api/tweets')
-async def create_new_tweet(api_key: str, tweet: Tweet):
-
-    return {"result": "true", "tweet_id": 1}
+async def create_new_tweet(api_key: str, tweet: schemas.Tweet,
+                           session: AsyncSession = Depends(get_session)) -> JSONResponse:
+    """
+    The endpoint creates a new tweet
+    :param api_key: the user's api_key. Needed for identification
+    :type api_key: str
+    :param tweet: tweet's data
+    :type tweet: schemas.schemas.Tweet
+    :param session: the session object for working with the database.
+    Provided by the Depends dependency function
+    :type session: AsyncSession
+    :return: json response with tweet_id
+    :rtype: JSONResponse
+    """
+    return JSONResponse(content={"result": "true", "tweet_id": 1},
+                        status_code=status.HTTP_201_CREATED)
