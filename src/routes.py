27d03a6b1f97c0c7eaef8e962489d5ac1b213d2b
vladimir <volosnikovvladimir69@gmail.com>
2024-09-05 11:31:37 +0500

    Add a lifespan function that creates a pool of database connections before starting and resets the pool after stopping

--- src/routes.py
+++ src/routes.py
@@ -1,9 +1,30 @@
+from contextlib import asynccontextmanager
+
 from fastapi import FastAPI
 
 from schemas.schemas import Tweet
+from database.database import engine, Base, Session
 
 
 app = FastAPI()
+
+
+@asynccontextmanager
+async def lifespan(app_: FastAPI):
+    # Startup
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+    yield
+    # Shutdown
+    await engine.dispose()
+
+
+async def get_session():
+    session = Session()
+    try:
+        yield session
+    finally:
+        await session.close()
 
 
 @app.post('/api/tweets')
