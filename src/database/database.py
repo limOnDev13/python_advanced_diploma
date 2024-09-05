8d22ea1aa01fac3ae90f2615f18ff073b92f9af0
vladimir <volosnikovvladimir69@gmail.com>
2024-09-05 11:20:10 +0500

    Add tables User, Tweet and Image

---
+++ src/database/database.py
@@ -0,0 +1,13 @@
+"""The module is responsible for the work of the database"""
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+from src.config.config import load_config, Config
+
+
+config: Config = load_config()
+DB_URL: str = f"postgresql+asyncpg://{config.db.admin}:{config.db.password}@localhost"
+engine = create_async_engine(DB_URL)
+Session = sessionmaker(engine, expire_on_commit=False, class_=AsyncSession)
+Base = declarative_base()
