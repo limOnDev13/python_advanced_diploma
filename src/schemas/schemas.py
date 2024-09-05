34978cf1c689f35519da8d1f9c7eec4a40112362
vladimir <volosnikovvladimir69@gmail.com>
2024-09-05 10:00:50 +0500

    Add Tweet schema

---
+++ src/schemas/schemas.py
@@ -0,0 +1,18 @@
+from pydantic import BaseModel, Field
+from typing import Optional, List
+
+
+class Tweet(BaseModel):
+    tweet_data: str = Field(
+        default=...,
+        title='The text of the tweet',
+        description='The text of the tweet',
+    )
+    tweet_media_ids: Optional[List[int]] = Field(
+        default=None,
+        title='List of image IDs',
+        description='List of image IDs. An optional parameter. '
+                    'The frontend will upload images there '
+                    'automatically when sending a tweet and'
+                    ' substitute the id from there in json.'
+    )
