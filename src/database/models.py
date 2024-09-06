5e72810007cebde3c231b6b0c0184234a8cc18b3
vladimir <volosnikovvladimir69@gmail.com>
2024-09-06 11:43:55 +0500

    Raname tables User -> users, Tweet -> tweets, Image -> images; add method to_json for User class

--- src/database/models.py
+++ src/database/models.py
@@ -1,40 +1,45 @@
 """The module is responsible for the database models (tables)"""
+from typing import Any
+
 from sqlalchemy import Column, Text, Integer, String, ForeignKey
 from sqlalchemy.orm import relationship
-from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession, async_sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
 
 from src.config.config import load_config, Config
 
 
 config: Config = load_config()
-DB_URL: str = f"postgresql+asyncpg://{config.db.admin}:{config.db.password}@localhost"
+DB_URL: str = f"postgresql+asyncpg://{config.db.user}:{config.db.password}@0.0.0.0:5432"
 engine = create_async_engine(DB_URL)
-Session = sessionmaker(engine, expire_on_commit=False, class_=AsyncSession)
 Base = declarative_base()
+Session = async_sessionmaker(engine, expire_on_commit=False, class_=AsyncSession)
 
 
 class User(Base):
-    __tablename__ = 'User'
+    __tablename__ = 'users'
 
     id = Column(Integer, primary_key=True, autoincrement=True)
     api_key = Column(String, unique=True, index=True)
     tweets = relationship("Tweet", back_populates="user", lazy='joined', cascade='all, delete, delete-orphan')
 
+    def to_json(self) -> dict[str, Any]:
+        return {c.name: getattr(self, c.name) for c in
+                self.__table__.columns}
+
 
 class Tweet(Base):
-    __tablename__ = 'Tweet'
+    __tablename__ = 'tweets'
 
     id = Column(Integer, primary_key=True, index=True)
     tweet_data = Column(Text, nullable=False)
-    user_id = Column(Integer, ForeignKey('User.id'))
+    user_id = Column(Integer, ForeignKey('users.id'))
     user = relationship("User", back_populates="tweets")
     images = relationship("Image", lazy='joined', cascade='all, delete, delete-orphan')
 
 
 class Image(Base):
-    __tablename__ = 'Image'
+    __tablename__ = 'images'
 
     id = Column(Integer, primary_key=True, index=True)
-    tweet_id = Column(Integer, ForeignKey("Tweet.id"))
+    tweet_id = Column(Integer, ForeignKey("tweets.id"))
