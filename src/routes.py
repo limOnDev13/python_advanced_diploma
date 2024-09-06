ecfef792b8790c89fd968e2cd6d140e23f8419dd
vladimir <volosnikovvladimir69@gmail.com>
2024-09-06 11:51:08 +0500

    Add logging; remove transaction from lifespan to get_session; add swagger docs for create_new_tweet; fix create_new_tweet

--- src/routes.py
+++ src/routes.py
@@ -1,34 +1,51 @@
 from contextlib import asynccontextmanager
+from typing import Optional
+from logging import getLogger
+from logging.config import dictConfig
 
-from fastapi import FastAPI, status, Depends
+from fastapi import FastAPI, status, Depends, Response
 from fastapi.responses import JSONResponse
+from fastapi_exceptions.exceptions import AuthenticationFailed
 from sqlalchemy.ext.asyncio import AsyncSession
+import uvicorn
 
 from schemas import schemas
-from database.database import engine, Base, Session
+from database.models import engine, Base, Session
 from database import queries as q
+from database import models
+from config.log_config import dict_config
+
+
+dictConfig(dict_config)
+logger = getLogger('routes_logger')
 
 
 @asynccontextmanager
-async def lifespan(app_: FastAPI):
+async def lifespan(app: FastAPI):
     # Startup
+    logger.info('Startup')
     async with engine.begin() as conn:
         await conn.run_sync(Base.metadata.create_all)
-
-    # Check that the user table has rows
-    session: AsyncSession = Session()
-    if not q.user_table_has_rows(session):
-        await q.create_user(session, {"api_key": "test_api_key"})
-
+    logger.debug('Before yield')
     yield
+    logger.debug('After yield')
     # Shutdown
+    logger.info('Shutdown')
     await engine.dispose()
 
 
 async def get_session():
+    logger.info('Getting session')
     session: AsyncSession = Session()
+    # Check that the user table has rows
+    if not await q.user_table_has_rows(session):
+        logger.debug("Table User doesn't have any rows => add test user")
+        await q.create_user(session, {"api_key": "test_api_key"})
+
     try:
+        logger.debug('Before yield session')
         yield session
+        logger.debug('After yield session')
     finally:
         await session.close()
 
@@ -36,20 +53,52 @@ async def get_session():
 app = FastAPI(lifespan=lifespan)
 
 
-@app.post('/api/tweets')
+@app.post(
+    '/api/tweets',
+    status_code=201,
+    responses={
+        401: {"description": "api_key not exists",
+              "content": {
+                  "application/json": {
+                      "example": {"result": False,
+                                  "error_type": "AuthenticationFailed",
+                                  "error_message": "api_key <api_key> not exists"
+                                  }
+                  }
+              }
+        },
+        201: {"description": "Tweet was created",
+              "content": {
+                  "application/json": {
+                      "example": {"result": True, "tweet_id": 1}
+                  }
+              }
+        }
+    }
+)
 async def create_new_tweet(api_key: str, tweet: schemas.Tweet,
-                           session: AsyncSession = Depends(get_session)) -> JSONResponse:
+                           response: Response,
+                           session: AsyncSession = Depends(get_session)):
     """
-    The endpoint creates a new tweet
-    :param api_key: the user's api_key. Needed for identification
-    :type api_key: str
-    :param tweet: tweet's data
-    :type tweet: schemas.schemas.Tweet
-    :param session: the session object for working with the database.
-    Provided by the Depends dependency function
-    :type session: AsyncSession
-    :return: json response with tweet_id
-    :rtype: JSONResponse
+    The endpoint creates a new tweet.
     """
-    return JSONResponse(content={"result": "true", "tweet_id": 1},
-                        status_code=status.HTTP_201_CREATED)
+    logger.info('Start creating a new tweet')
+    user_id: Optional[models.User] = await q.get_user_id_by_api_key(session, api_key)
+    if not user_id:
+        logger.warning(f'api_key {api_key} not exists')
+        response.status_code = status.HTTP_403_FORBIDDEN
+        return JSONResponse(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            content={"result": False,
+                     "error_type": "AuthenticationFailed",
+                     "error_message": f"api_key {api_key} not exists"})
+
+    logger.debug('api_key exists')
+    tweet_id: int = await q.create_tweet(session, user_id[0], tweet.model_dump())
+    logger.debug(f'Tweet was created, tweet_id={tweet_id}')
+    response.status_code = status.HTTP_201_CREATED
+    return {"result": "true", "tweet_id": tweet_id}
+
+
+if __name__ == '__main__':
+    uvicorn.run(app)
