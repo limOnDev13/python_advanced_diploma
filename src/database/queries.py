8cb1f7180db49b6e2a2ea3f011be700cd078c8bd
vladimir <volosnikovvladimir69@gmail.com>
2024-09-06 11:47:29 +0500

    Add logging; Remove transactions from funcs create_user and create_tweet (transaction is created when accessing the endpoint); fix func create_tweet

--- src/database/queries.py
+++ src/database/queries.py
@@ -1,10 +1,15 @@
 """The module is responsible for database queries"""
-from typing import Optional, List
+from typing import Optional, List, Dict
+from logging import Logger
 
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from models import User, Image, Tweet
+from src.database.models import User, Image, Tweet
+
+
+logger = Logger('query_logger')
+logger.setLevel('DEBUG')
 
 
 async def user_table_has_rows(session: AsyncSession) -> bool:
@@ -19,52 +24,57 @@ async def user_table_has_rows(session: AsyncSession) -> bool:
     return exists.first() is not None
 
 
-async def create_user(session: AsyncSession, user_dict: dict[str, str]) -> None:
+async def create_user(session: AsyncSession, user_dict: Dict[str, str]) -> None:
     """
     The function adds the user to the database
     :param session: session object
     :type session: AsyncSession
     :param user_dict: The user's data
-    :type user_dict: dict[str, str]
+    :type user_dict: Dict[str, str]
     :return: None
     """
-    async with session.begin():
-        session.add(User(**user_dict))
-        await session.commit()
+    session.add(User(**user_dict))
+    await session.commit()
 
 
-async def get_user_by_api_key(session: AsyncSession, api_key: str) -> Optional[User]:
+async def get_user_id_by_api_key(session: AsyncSession, api_key: str) -> Optional[int]:
     """
     Function for getting user by api_key
     :param session: session object
     :param api_key: user's api_key
     :type api_key: str
-    :return: User, if such api_key exists, else None
-    :rtype: Optional[User]
+    :return: user_id, if such api_key exists, else None
+    :rtype: Optional[int]
     """
-    user = await session.execute(select(User).where(User.api_key == api_key))
+    user = await session.execute(select(User.id).where(User.api_key == api_key))
     return user.first()
 
 
-async def create_tweet(session: AsyncSession, user: User, tweet_data: dict) -> None:
+async def create_tweet(session: AsyncSession, user_id: int, tweet_data: Dict) -> int:
     """
     Function for creating tweet
     :param session: session object
-    :param user: user object
-    :type user: User
+    :param user_id: user object
+    :type user_id: User
     :param tweet_data: tweet's data
-    :type tweet_data: dict
-    :return: None
+    :type tweet_data: Dict
+    :return: tweet_id
+    :rtype: int
     """
-    async with session.begin():
-        tweet_data["user_id"] = user.id
-        # if there are images, we will attach them to the tweet
-        if "tweet_media_ids" in tweet_data:
-            images_ids: List[int] = tweet_data.pop("tweet_media_ids")
-            images_res = await session.execute(select(Image).where(Image.id.in_(images_ids)))
-            images = images_res.all()
+    logger.info('Start creating tweet')
+    print('Start creating tweet')
+    tweet_data['user_id'] = user_id
+    images_ids: Optional[List[int]] = tweet_data.pop("tweet_media_ids")
 
-        tweet: Tweet = Tweet(**tweet_data)
-        tweet.images = images
-        user.tweets.append(tweet)
-        await session.commit()
+    images = list()
+    # if there are images, we will attach them to the tweet
+    if images_ids:
+        images_q = await session.execute(select(Image).where(Image.id.in_(images_ids)))
+        images = images_q.all()
+    tweet_data['images'] = images
+
+    tweet = Tweet(**tweet_data)
+    session.add(tweet)
+    await session.commit()
+
+    return tweet.id
