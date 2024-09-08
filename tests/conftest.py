0baca7be4cc4218745005c4704b089876e5b770c
vladimir <volosnikovvladimir69@gmail.com>
2024-09-08 14:57:24 +0500

    After linters

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,8 +6,8 @@ from fastapi import FastAPI
 from httpx import ASGITransport, AsyncClient
 from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
 
-from ..src.database.models import DB_URL, Base, User
-from ..src.routes import create_app, get_session
+from src.database.models import DB_URL, Base, User
+from src.routes import create_app, get_session
 
 
 @pytest_asyncio.fixture(scope="function")
