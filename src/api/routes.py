3fb04a762e6e7b5f1ce6b51e6f229ac152bb22f3
vladimir <volosnikovvladimir69@gmail.com>
2024-09-11 14:57:11 +0500

    Put the general part of the functionality of the endpoints in a separate service/web module (get_session, lifespan); add endpoint /aoi/medias for uploading images

--- src/api/routes.py
+++ src/api/routes.py
@@ -1,52 +1,23 @@
-from contextlib import asynccontextmanager
 from logging import getLogger
 from logging.config import dictConfig
-from typing import Optional
 
 import uvicorn
-from fastapi import Depends, FastAPI, Response, status
+from fastapi import Depends, FastAPI, Response, status, UploadFile, File
 from fastapi.responses import JSONResponse
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from src.config.log_config import dict_config
 from src.database import queries as q
-from src.database.models import Base, Session, engine
+
 from src.schemas import schemas
+from src.service.images import upload_image
+from src.service.web import lifespan, get_session, json_response_with_error, check_api_key
 
 dictConfig(dict_config)
 logger = getLogger("routes_logger")
 
 
-async def get_session():
-    logger.info("Getting session")
-    session: AsyncSession = Session()
-    # Check that the user table has rows
-    if not await q.user_table_has_rows(session):
-        logger.debug("Table User doesn't have any rows => add test user")
-        await q.create_user(session, {"api_key": "test_api_key"})
-
-    try:
-        logger.debug("Before yield session")
-        yield session
-        logger.debug("After yield session")
-    finally:
-        await session.close()
-
-
 def create_app() -> FastAPI:
-    @asynccontextmanager
-    async def lifespan(app: FastAPI):
-        # Startup
-        logger.info("Startup")
-        async with engine.begin() as conn:
-            await conn.run_sync(Base.metadata.create_all)
-        logger.debug("Before yield")
-        yield
-        logger.debug("After yield")
-        # Shutdown
-        logger.info("Shutdown")
-        await engine.dispose()
-
     app = FastAPI(lifespan=lifespan)
 
     @app.post(
@@ -83,24 +54,65 @@ def create_app() -> FastAPI:
         The endpoint creates a new tweet.
         """
         logger.info("Start creating a new tweet")
-        user_id: Optional[int] = await q.get_user_id_by_api_key(session, api_key)
-        if not user_id:
-            logger.warning("api_key %s not exists", api_key)
-            response.status_code = status.HTTP_403_FORBIDDEN
-            return JSONResponse(
-                status_code=status.HTTP_401_UNAUTHORIZED,
-                content={
-                    "result": False,
-                    "error_type": "AuthenticationFailed",
-                    "error_message": f"api_key {api_key} not exists",
-                },
-            )
+        user_id: int | JSONResponse = await check_api_key(api_key, session, response)
+        # if api_key is in the database, the function will return user_id,
+        # otherwise it will return JSON Response - we will return it immediately
+        if not isinstance(user_id, int):
+            return user_id
 
         logger.debug("api_key exists")
         tweet_id: int = await q.create_tweet(session, user_id, tweet.model_dump())
         logger.debug("Tweet was created, tweet_id=%d", tweet_id)
         response.status_code = status.HTTP_201_CREATED
         return {"result": "true", "tweet_id": tweet_id}
+
+    @app.post(
+        "/api/medias",
+        status_code=201,
+        responses={
+            401: {
+                "description": "api_key not exists",
+                "content": {
+                    "application/json": {
+                        "example": {
+                            "result": False,
+                            "error_type": "AuthenticationFailed",
+                            "error_message": "api_key <api_key> not exists",
+                        }
+                    }
+                },
+            },
+            201: {
+                "description": "Image was saved",
+                "content": {
+                    "application/json": {"example": {"result": True, "media_id": 1}}
+                },
+            },
+        },
+    )
+    async def save_image(
+        api_key: str,
+        image: UploadFile,
+        response: Response,
+        session: AsyncSession = Depends(get_session),
+    ):
+        """
+        The endpoint saves the image to disk
+        """
+        logger.info(f'Start saving image')
+        user_id: int | JSONResponse = await check_api_key(api_key, session, response)
+        # if api_key is in the database, the function will return user_id,
+        # otherwise it will return JSON Response - we will return it immediately
+        if not isinstance(user_id, int):
+            return user_id
+
+        try:
+            image_id: int = await upload_image(image, session)
+            return {"result": True, "image_id": image_id}
+        except Exception as exc:
+            return json_response_with_error(exc)
+        finally:
+            await image.close()
 
     return app
 
