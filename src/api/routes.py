91d9bb316dc97b4e39ffbd96ab11d403da2bc55e
vladimir <volosnikovvladimir69@gmail.com>
2024-09-12 16:59:04 +0500

    Add logging into endpoint /api/medias; add validation of image into endpoint /api/medias; add validation of images_ids into endpoint /api/tweets; add docs about new validations into all endpoints

--- src/api/routes.py
+++ src/api/routes.py
@@ -1,17 +1,20 @@
 from logging import getLogger
 from logging.config import dictConfig
 
-import uvicorn
-from fastapi import Depends, FastAPI, Response, status, UploadFile, File
+from fastapi import Depends, FastAPI, Response, UploadFile, status
 from fastapi.responses import JSONResponse
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from src.config.log_config import dict_config
 from src.database import queries as q
-
 from src.schemas import schemas
-from src.service.images import upload_image
-from src.service.web import lifespan, get_session, json_response_with_error, check_api_key
+from src.service.images import upload_image, validate_image, validate_images_in_db
+from src.service.web import (
+    check_api_key,
+    get_session,
+    json_response_with_error,
+    lifespan,
+)
 
 dictConfig(dict_config)
 logger = getLogger("routes_logger")
@@ -32,6 +35,18 @@ def create_app() -> FastAPI:
                             "result": False,
                             "error_type": "AuthenticationFailed",
                             "error_message": "api_key <api_key> not exists",
+                        }
+                    }
+                },
+            },
+            403: {
+                "description": "Some image ids not exist or relate other tweets",
+                "content": {
+                    "application/json": {
+                        "example": {
+                            "result": False,
+                            "error_type": "ValueError",
+                            "error_message": "error message",
                         }
                     }
                 },
@@ -58,9 +73,18 @@ def create_app() -> FastAPI:
         # if api_key is in the database, the function will return user_id,
         # otherwise it will return JSON Response - we will return it immediately
         if not isinstance(user_id, int):
+            logger.warning("api_key not found")
             return user_id
+        logger.debug("Identification is successful")
 
-        logger.debug("api_key exists")
+        # validate images_ids - images must be in db and not relate other tweets
+        if tweet.tweet_media_ids:
+            try:
+                await validate_images_in_db(session, tweet.tweet_media_ids)
+            except ValueError as exc:
+                logger.debug("Some image ids not exists or relate other tweets")
+                return json_response_with_error(exc, 403)
+
         tweet_id: int = await q.create_tweet(session, user_id, tweet.model_dump())
         logger.debug("Tweet was created, tweet_id=%d", tweet_id)
         response.status_code = status.HTTP_201_CREATED
@@ -70,6 +94,18 @@ def create_app() -> FastAPI:
         "/api/medias",
         status_code=201,
         responses={
+            400: {
+                "description": "Something went wrong when uploading the image",
+                "content": {
+                    "application/json": {
+                        "example": {
+                            "result": False,
+                            "error_type": "error type",
+                            "error_message": "error message",
+                        }
+                    }
+                },
+            },
             401: {
                 "description": "api_key not exists",
                 "content": {
@@ -78,6 +114,18 @@ def create_app() -> FastAPI:
                             "result": False,
                             "error_type": "AuthenticationFailed",
                             "error_message": "api_key <api_key> not exists",
+                        }
+                    }
+                },
+            },
+            403: {
+                "description": "Image size too large or image has invalid format",
+                "content": {
+                    "application/json": {
+                        "example": {
+                            "result": False,
+                            "error_type": "error type",
+                            "error_message": "error message",
                         }
                     }
                 },
@@ -99,24 +147,34 @@ def create_app() -> FastAPI:
         """
         The endpoint saves the image to disk
         """
-        logger.info(f'Start saving image')
+        logger.info("Start saving image")
         user_id: int | JSONResponse = await check_api_key(api_key, session, response)
         # if api_key is in the database, the function will return user_id,
         # otherwise it will return JSON Response - we will return it immediately
         if not isinstance(user_id, int):
+            logger.warning("api_key not found")
             return user_id
+        logger.debug("Identification is successful")
 
         try:
+            logger.debug("Validating image")
+            validate_image(image)
+
+            logger.debug("Trying to upload an image")
             image_id: int = await upload_image(image, session)
+            logger.debug("Image was uploaded, image_id=%s", image_id)
+
             return {"result": True, "image_id": image_id}
+        except ValueError as exc:
+            logger.warning("Image too large", exc_info=exc)
+            return json_response_with_error(exc, 403)
+        except TypeError as exc:
+            logger.warning("Wrong image format", exc_info=exc)
+            return json_response_with_error(exc, 403)
         except Exception as exc:
-            return json_response_with_error(exc)
+            logger.exception("Smth wrong", exc_info=exc)
+            return json_response_with_error(exc, 400)
         finally:
             await image.close()
 
     return app
-
-
-if __name__ == "__main__":
-    app_ = create_app()
-    uvicorn.run(app_)
