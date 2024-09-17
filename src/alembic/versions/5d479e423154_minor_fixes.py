0a767ac6f7e3a7f087c323f2143a80188da86173
Vladimir Volosnikov <volosnikovvladimir69@gmail.com>
2024-09-17 17:17:44 +0500

    After linters

--- src/alembic/versions/5d479e423154_minor_fixes.py
+++ src/alembic/versions/5d479e423154_minor_fixes.py
@@ -5,15 +5,16 @@ Revises: bf8194eb5fbe
 Create Date: 2024-09-17 15:46:03.819813
 
 """
+
 from typing import Sequence, Union
 
-from alembic import op
 import sqlalchemy as sa
 
+from alembic import op
 
 # revision identifiers, used by Alembic.
-revision: str = '5d479e423154'
-down_revision: Union[str, None] = 'bf8194eb5fbe'
+revision: str = "5d479e423154"
+down_revision: Union[str, None] = "bf8194eb5fbe"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
--- src/alembic/versions/bf8194eb5fbe_add_table_likes_and_foreignkeys_for_.py
+++ src/alembic/versions/bf8194eb5fbe_add_table_likes_and_foreignkeys_for_.py
@@ -5,15 +5,16 @@ Revises: a35905adef8c
 Create Date: 2024-09-17 15:31:33.417537
 
 """
+
 from typing import Sequence, Union
 
-from alembic import op
 import sqlalchemy as sa
 
+from alembic import op
 
 # revision identifiers, used by Alembic.
-revision: str = 'bf8194eb5fbe'
-down_revision: Union[str, None] = 'a35905adef8c'
+revision: str = "bf8194eb5fbe"
+down_revision: Union[str, None] = "a35905adef8c"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
