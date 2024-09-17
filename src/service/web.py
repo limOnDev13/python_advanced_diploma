0f766cc1fb0c2c98cea951874c50e3111b8fd5e9
Vladimir Volosnikov <volosnikovvladimir69@gmail.com>
2024-09-17 12:26:53 +0500

    Remove json_response_with_error; add validation that the user deletes their tweet; fix check_api_key

--- src/service/web.py
+++ src/service/web.py
@@ -2,13 +2,12 @@ from contextlib import asynccontextmanager
 from logging import getLogger
 from typing import Optional
 
-from fastapi import FastAPI, Response, status
-from fastapi.responses import JSONResponse
-from fastapi_exceptions.exceptions import AuthenticationFailed, NotFound
+from fastapi import FastAPI, Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from src.database import queries as q
 from src.database.models import Base, Session, Tweet, engine
+from src.service.exceptions import IdentificationError, NotFoundError, ForbiddenError
 
 logger = getLogger("routes_logger")
 
@@ -43,50 +42,35 @@ async def get_session():
         await session.close()
 
 
-def json_response_with_error(exc: Exception, status_code: int = 400) -> JSONResponse:
-    """
-    The function returns a JsonResponse in case of an error
-    :param exc: Exception
-    :param status_code: HTTP code
-    :return: JSONResponse
-    """
-    return JSONResponse(
-        status_code=status_code,
-        content={
-            "result": False,
-            "error_type": type(exc).__name__,
-            "error_message": str(exc),
-        },
-    )
-
-
-async def check_api_key(
-    api_key: str, session: AsyncSession, response: Response
-) -> int | JSONResponse:
+async def check_api_key(api_key: str, session: AsyncSession = Depends(get_session)) -> int:
     """
     The function checks if the given api_key is in the database.
     :param api_key: The key is the user ID
-    :param session: session object
-    :param response: object of the response
-    :return: user_id (int) if api_key is in the database,
-    otherwise JSONResponse with an identification error response
-    :rtype: int | JSONResponse
+    :param session: async session object
+    :raise HTTPException: if api_key is not in database
+    :return: user_id (int) if api_key is in the database
+    :rtype: int
     """
+    logger.info("Start checking api_key")
     user_id: Optional[int] = await q.get_user_id_by_api_key(session, api_key)
+
     if not user_id:
-        logger.warning("api_key %s not exists", api_key)
-        response.status_code = status.HTTP_401_UNAUTHORIZED
-        return json_response_with_error(
-            AuthenticationFailed(f"api_key {api_key} not exists"), 401
-        )
+        logger.warning("api_key not exists")
+        raise IdentificationError("api_key not exists")
+
+    logger.debug("Identification is successful")
     return user_id
 
 
 async def check_tweet_id(
-    tweet_id: int, session: AsyncSession
-) -> Optional[JSONResponse]:
+    tweet_id: int, user_id: int, session: AsyncSession
+) -> None:
     """Function check tweet_id"""
+    # Check that tweet_id exists
     tweet: Optional[Tweet] = await q.get_tweet_by_id(session, tweet_id)
     if not tweet:
-        return json_response_with_error(NotFound(f"tweet_id {tweet_id} not found"), 404)
-    return None
+        raise NotFoundError(f"tweet_id {tweet_id} not found")
+
+    # check that tweet relates user_id
+    if tweet.user_id != user_id:
+        raise ForbiddenError(f"The tweet {tweet_id} does not belong to user {user_id}")
