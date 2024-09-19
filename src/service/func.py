c1c68ea6a6ccaa4d837e3517d49339864707d5ae
Vladimir Volosnikov <volosnikovvladimir69@gmail.com>
2024-09-19 15:44:14 +0500

    Remove func get_image_name_by_id from src.service.images.py into src.service.func.py (cause of cyclic import)

---
+++ src/service/func.py
@@ -0,0 +1,23 @@
+from typing import Optional
+from logging import getLogger
+import re
+
+import aiofiles.os
+
+
+logger = getLogger("image_logger.func")
+
+
+async def get_image_name_by_id(image_id, images_path) -> Optional[str]:
+    """Function returns image name by id"""
+    logger.info(
+        "Start searching image name with id %d in dir %s", image_id, images_path
+    )
+    for filename in await aiofiles.os.listdir(images_path):
+        logger.debug("Current filename - %s", filename)
+        if re.fullmatch(rf"{image_id}\..*?$", filename):
+            logger.debug("The file %s fits - returns", filename)
+            return filename
+        logger.debug("The file %s does not fit", filename)
+    logger.warning("No matches")
+    return None
